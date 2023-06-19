7547b597dc75b46ec6e4afe0bb3c7f93ddb6749b
Angelo <castromatheus060@gmail.com>
2023-06-19 15:02:10 -0300

    feat(main): added validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080 || process.env.PORT);
 }
 bootstrap();
